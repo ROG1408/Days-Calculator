3d7c64645e6d0776339acc9f574f44d5ffb3eacf
ROG1408 <rog.sj408@gmail.com>
2025-12-29 21:40:47 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
+const CACHE_NAME = "day-until-calc";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("dayscalc-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
